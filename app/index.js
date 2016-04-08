6715f81452ddd5c39cbbb70f7ed2940253a9d62d
Davide Petrillo <petr@illodavi.de>
2016-04-08 14:16:28 +0100

    Added handling of fields and form change

--- app/index.js
+++ app/index.js
@@ -1,32 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var CardEntry = React.createClass({
+var CardForm = React.createClass({
+  getInitialState: function() {
+    return {
+      front: '',
+      back: ''
+    };
+  },
+  handleFrontChange: function(e) {
+    console.log(e);
+    this.setState({front: e.target.value});
+  },
+  handleBackChange: function(e) {
+    this.setState({back: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    console.log(this.state);
+  },
   render: function() {
     return (
-      <div className="cardEntry">
-        <label>Front</label>
-        <input type="text" name="front" />
-        <label>Back</label>
-        <input type="text" name="back" />
+      <div className="cardForm">
+        <h2>Add Card</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>Front</label>
+          <input type="text" name="front" onChange={this.handleFrontChange} />
+          <label>Back</label>
+          <input type="text" name="back"  onChange={this.handleBackChange} />
+          <input type="submit" value="Save" />
+        </form>
       </div>
     );
   }
 });
-
-var CardForm = React.createClass({
-  render: function() {
-    return (
-      <form className="cardForm">
-        <h2>Add Card</h2>
-        <CardEntry />
-        <input type="submit" value="Save" />
-      </form>
-    );
-  }
-});
-
-console.log('working');
 
 
 ReactDOM.render(
